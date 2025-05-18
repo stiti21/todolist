d4b6251cf11bb6dc02f606fd02b70e49a1f65173
mahasen03 <122237364+mahasen03@users.noreply.github.com>
2025-05-18 19:56:19 +0300

    Update TaskManager.java

--- src/main/java/com/mycompany/finalkanbanwithreminders/TaskManager.java
+++ src/main/java/com/mycompany/finalkanbanwithreminders/TaskManager.java
@@ -11,39 +11,3 @@
  * It also maintains a list of all tasks currently managed in the application.
  * TaskManager acts as the main connection between the GUI and the data layer.
  */
-
-
-package com.mycompany.finalkanbanwithreminders;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class TaskManager {
-    private final List<Task> tasks;
-
-    public TaskManager() {
-        tasks = new ArrayList<>();
-    }
-
-    // Modified to accept Task directly
-    public void addTask(Task task) {
-        tasks.add(task);
-        System.out.println("Task added: " + task.getDescription());
-    }
-
-    public void removeTask(String description) {
-        tasks.removeIf(task -> task.getDescription().equals(description));
-        System.out.println("Task removed: " + description);
-    }
-
-    public void displayAllTasks() {
-        System.out.println("Current Tasks:");
-        for (Task task : tasks) {
-            System.out.println("- " + task.getDescription() + " | Priority: " + task.getPriority());
-        }
-    }
-
-    public List<Task> getAllTasks() {
-        return tasks;
-    }
-}
