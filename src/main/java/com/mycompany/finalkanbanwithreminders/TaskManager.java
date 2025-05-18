9ec6c5883d47a04e845f7789c542190ae9ebeb04
mahasen03 <122237364+mahasen03@users.noreply.github.com>
2025-05-18 03:14:03 +0300

    Update TaskManager.java

--- src/main/java/com/mycompany/finalkanbanwithreminders/TaskManager.java
+++ src/main/java/com/mycompany/finalkanbanwithreminders/TaskManager.java
@@ -5,6 +5,13 @@
 // - Sulaf Ahmed Al-Titi - 2021904034
 // - Ruqaia  Manwar - 2023905070
 // -----------------------------------------------------
+/*
+ * This class handles the core logic for managing tasks in the application.
+ * It allows adding, removing, and listing tasks.
+ * It also maintains a list of all tasks currently managed in the application.
+ * TaskManager acts as the main connection between the GUI and the data layer.
+ */
+
 
 package com.mycompany.finalkanbanwithreminders;
 
